f26023d746ffa17155df7728333477167ca92849
hui-home-desktop <yihaozhadan@gmail.com>
2020-09-27 12:01:07 -0700

    Upgrade node dependencies

--- store.js
+++ store.js
@@ -1,12 +1,12 @@
 import {applyMiddleware, createStore} from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension';
-import {createLogger} from 'redux-logger';
+import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import promise from 'redux-promise-middleware';
 import { VisibilityFilters } from './actions';
 import testCaseReducer from './reducers';
 
-const middleware = applyMiddleware(promise(), thunk, createLogger());
+const middleware = applyMiddleware(promise, thunk, logger);
 const testSuitesInitialState = {
   visibilityFilter: VisibilityFilters.SHOW_ALL,
   testSuites: {
